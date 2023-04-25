3b813170fc857c4e9260c23a0313fce9fe16bf2a
Ricardo-SS <ricardo.ufersa.pdf@gmail.com>
2023-04-25 17:25:50 -0300

    curso finalizado

--- aluramidi/js/main.js
+++ aluramidi/js/main.js
@@ -1,7 +1,39 @@
-function tocaSomPom (){
-    document.querySelector('#som_tecla_pom').play();
+function tocaSom(idElementoAudio) {
+    const elemento = document.querySelector(idElementoAudio);
+
+
+    if (elemento != null && elemento.localName === 'audio') {
+        elemento.play();
+    } 
+    else {
+        console.log('elemento não existe');
+    }
 }
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
-listaDeTeclas[0].onclick = tocaSomPom
+
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
+
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+    //montando string
+    const idAudio = `#som_${instrumento}`;
+
+    console.log(idAudio);
+    tecla.onclick = function () {
+        tocaSom(idAudio);
+    }
+    //console.log(contador);
+    tecla.onkeydown = function (evento) {
+
+        //console.log(evento.code);
+
+        if (evento.code === 'Space' || evento.code === 'Enter') {
+            tecla.classList.add('ativa');
+        }
+    }
+    tecla.onkeyup = function () {
+        tecla.classList.remove('ativa')
+    }
+}
